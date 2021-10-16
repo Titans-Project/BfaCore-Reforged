bcf493362aa3016816f6473ea01944a43f0a0ed4
SPP User <spp-user@spp-forum.de>
2021-10-16 18:34:11 +0300

    DB/SAI: quick fix Razorfen Downs
    (cherry picked from commit 5ade5d530785092d677f8428404b1068d5cb3ac9)

--- sql/updates/world/2021_15_10_00_world.sql
+++ sql/updates/world/2021_15_10_00_world.sql
@@ -34,7 +34,8 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,0,0,100,0,3000,5000,12000,15000,11,151810,0,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Frozen Bomb'),
-(@ENTRY,0,1,0,2,0,100,0,0,40,22000,25000,11,150679,2,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Soul Leech at 40% HP');
+(@ENTRY,0,1,0,2,0,100,0,0,40,22000,25000,11,150679,2,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Soul Leech at 40% HP'),
+(@ENTRY,0,2,0,4,0,100,1,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Say Text on Aggro');
 
 -- Death Speaker Blackthorn
 SET @ENTRY := 74875;
@@ -51,9 +52,13 @@ SET @ENTRY := 74347;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,0,0,0,100,0,0,0,2000,2000,11,150001,64,0,0,0,0,2,0,0,0,0,0,0,0,'Cast Lava Burst'),
-(@ENTRY,0,1,0,2,0,100,0,0,40,22000,25000,11,150067,2,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Summon Blazing Servitor at 40% HP'),
-(@ENTRY,0,2,0,9,0,100,0,0,5,15000,25000,11,151575,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Unleashed Flames on Close');
+(@ENTRY,'0','0','0','10','0','100','1','0','25','0','0','1','0','0','0','0','0','0','1','0','0','0','0','0','0','0','SmartAI'),
+(@ENTRY,'0','1','0','4','0','100','0','0','0','0','0','1','1','0','0','0','0','0','1','0','0','0','0','0','0','0','SmartAI'),
+(@ENTRY,'0','2','3','6','0','100','0','0','0','0','0','1','2','0','0','0','0','0','1','0','0','0','0','0','0','0','SmartAI'),
+(@ENTRY,'0','3','0','61','0','100','0','0','0','0','0','9','0','0','0','0','0','0','15','224819','1000','0','0','0','0','0','SmartAI'),
+(@ENTRY,'0','4','0','0','0','100','0','15000','15000','25000','30000','11','150067','0','0','0','0','0','1','0','0','0','0','0','0','0','SmartAI'),
+(@ENTRY,'0','5','0','0','0','100','0','10000','10000','25000','35000','11','151575','0','0','0','0','0','1','0','0','0','0','0','0','0','SmartAI'),
+(@ENTRY,'0','6','0','0','0','100','0','5000','7000','15000','25000','11','150001','0','0','0','0','0','2','0','0','0','0','0','0','0','SmartAI');
 
 -- Mushlump
 SET @ENTRY := 74435;
@@ -208,138 +213,3 @@ UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,11,0,100,1,0,0,0,0,11,22696,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Thorns on Spawn');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
