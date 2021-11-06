cc6b585f3a8f28d54b8a30a1727c68a964f683cf
Aphelios <25911899+Nexon99@users.noreply.github.com>
2021-11-06 19:32:02 +0100

    Database/Creature : fix faction

---
+++ sql/updates/world/2021_11_06_world_Creature_template_fix_faction.sql
@@ -0,0 +1,8 @@
+/*
+**************************
+*    BfaCore Reforged    *
+**************************
+*/
+
+UPDATE creature_template SET faction = 72 WHERE entry = 148073;
+UPDATE creature_template SET faction = 72 WHERE  faction = 469;
