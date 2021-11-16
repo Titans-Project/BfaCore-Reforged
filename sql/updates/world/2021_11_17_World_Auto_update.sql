a23090c542a4f5194edfe0a455002512b53f3243
Aphelios <25911899+Nexon99@users.noreply.github.com>
2021-11-17 00:08:27 +0100

    Database/World : Auto update

---
+++ sql/updates/world/2021_11_17_World_Auto_update.sql
@@ -0,0 +1,7 @@
+/*
+**************************
+*    BfaCore Reforged    *
+**************************
+*/
+
+UPDATE `world_test`.`updates_include` SET `path`='$/sql/updates/world' WHERE  `path`='$/sql/LatincoreBfa/world';
