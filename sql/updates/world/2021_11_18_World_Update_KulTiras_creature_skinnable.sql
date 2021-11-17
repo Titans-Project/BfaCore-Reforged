461e5a535cdb4021a6ce3dea5fd37351972e6cee
Aphelios <25911899+Nexon99@users.noreply.github.com>
2021-11-17 17:14:43 +0100

    Database/Creature : Fix skinnable creatures for Kul'Tiras

---
+++ sql/updates/world/2021_11_18_World_Update_KulTiras_creature_skinnable.sql
@@ -0,0 +1,93 @@
+/*
+**************************
+*    BfaCore Reforged    *
+**************************
+*/
+
+-- Creature Skinnable
+
+-- Riverland Doe
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 142183);
+
+-- Riverland Stag
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 142195);
+
+-- Coppertail Fox
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 131387);
+-- Bucktooth Beaver
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 134359);
+
+-- Stonejaw Saurolisk
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 141641);
+
+-- Stonejaw Matriarch
+UPDATE `creature_template` SET `unit_flags` = 67141696  WHERE (`entry` = 141696);
+
+-- Cragbound Fox
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 126455);
+
+-- Noblehoof Stag
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 126708);
+
+-- Noblehoof Doe
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 126709);
+
+-- Mudbrush Boar
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 125347);
+
+-- Tamed Boar
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 127699);
+
+-- Hexcrazed Doe
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 129764);
+
+-- Hexcrazed Greatstag
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 128361);
+
+-- Hexed Thornclaw
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 142278);
+
+-- Crimsonwood Prowler1
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 137640);
+
+-- Mountain Greatstag
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 127806);
+
+-- Mountain Doe
+UPDATE `creature_template` SET `unit_flags` = 67142400  WHERE (`entry` = 127807);
+
+-- Scavenging Boar
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 133822);
+
+-- Mountain Goat
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 134349);
+
+-- Feral Razorclaw
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 136416);
+
+-- Baby Goat
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 134350);
+
+-- Mother Goat
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 136939);
+
+-- Volatile Rat
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 137126);
+
+-- Bilge Rat
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 136686);
+
+-- Barbedclaw Grizzly
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 139793);
+
+-- Coldtooth Alpha
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 136204);
+
+-- Coldtooth Pup
+UPDATE `creature_template` SET `unit_flags` = 67403776  WHERE (`entry` = 136206);
+
+-- Coldtooth Howler
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 136205);
+
+-- Void-Touched Fox
+UPDATE `creature_template` SET `unit_flags` = 67141632  WHERE (`entry` = 142198);
