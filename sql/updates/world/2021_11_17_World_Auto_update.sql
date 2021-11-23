4ee8844ff6f8e4bac722237d000b883c60cdc4eb
Aphelios <25911899+Nexon99@users.noreply.github.com>
2021-11-24 00:40:38 +0100

    Fix mistake Auto Update
    oups

--- sql/updates/world/2021_11_17_World_Auto_update.sql
+++ sql/updates/world/2021_11_17_World_Auto_update.sql
@@ -4,4 +4,4 @@
 **************************
 */
 
-UPDATE `world_test`.`updates_include` SET `path`='$/sql/updates/world' WHERE  `path`='$/sql/LatincoreBfa/world';
+UPDATE `updates_include` SET `path`='$/sql/updates/world' WHERE  `path`='$/sql/LatincoreBfa/world';
