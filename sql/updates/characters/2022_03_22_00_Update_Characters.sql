99cdf06a7e0d9be1ae3bd4248016e41ba4ee226c
DuelistRag3 <christian.dullin@web.de>
2022-03-22 11:26:20 +0100

    Update

--- sql/updates/characters/2022_03_22_00_Update_Characters.sql
+++ sql/updates/characters/2022_03_22_00_Update_Characters.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `bfa_characters`.`characters` 
-ADD COLUMN `camp_x` int(10) NULL DEFAULT NULL AFTER `numRespecs`;
-ADD COLUMN `camp_y` int(10) NULL DEFAULT NULL AFTER `camp_x`;
-ADD COLUMN `camp_z` int(10) NULL DEFAULT NULL AFTER `camp_y`;
-ADD COLUMN `camp_o` int(10) NULL DEFAULT NULL AFTER `camp_z`;
+ADD COLUMN `camp_x` int(10) NULL DEFAULT NULL AFTER `numRespecs`,
+ADD COLUMN `camp_y` int(10) NULL DEFAULT NULL AFTER `camp_x`,
+ADD COLUMN `camp_z` int(10) NULL DEFAULT NULL AFTER `camp_y`,
+ADD COLUMN `camp_o` int(10) NULL DEFAULT NULL AFTER `camp_z`,
 ADD COLUMN `camp_mapid` int(10) NULL DEFAULT NULL AFTER `camp_o`;
