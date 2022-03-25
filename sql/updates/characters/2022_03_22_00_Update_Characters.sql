88c0cd03d159d52ee9842740b4c11f3a0f9ac5b1
DuelistRag3 <christian.dullin@web.de>
2022-03-25 20:36:09 +0100

    Removed static database name

--- sql/updates/characters/2022_03_22_00_Update_Characters.sql
+++ sql/updates/characters/2022_03_22_00_Update_Characters.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `bfa_characters`.`characters` 
+ALTER TABLE `characters` 
 ADD COLUMN `camp_x` int(10) NULL DEFAULT NULL AFTER `numRespecs`,
 ADD COLUMN `camp_y` int(10) NULL DEFAULT NULL AFTER `camp_x`,
 ADD COLUMN `camp_z` int(10) NULL DEFAULT NULL AFTER `camp_y`,
